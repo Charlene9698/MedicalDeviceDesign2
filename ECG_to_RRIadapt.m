0cd1262f724e6c9d004385bb7c351221ca3366c2
Charleneolive <35939804+Charlene9698@users.noreply.github.com>
2018-10-25 17:11:49 +0100

    Add files via upload

--- ECG_to_RRIadapt.m
+++ ECG_to_RRIadapt.m
@@ -1,99 +1,99 @@
-function [xRRI, yECG, ANOMALIES] =ECG_to_RRIadapt(varargin)
 
-%Adapted from  David Looney, modified to suit project
-% -------------------------------------------------------------------------
-% Syntax: 
-%          [xRRI,fsRRI]=ECG_to_RRI(xECG,fsECG);
-% -------------------------------------------------------------------------
-% Description:
-%
-% Converts ECG time series input into RR Interval time series.
-% 
-% Type of artifacts and noise on the ECG:
-% 1. Muscle: 5 to 50 Hz
-% 2. Respiratory: 0.12 -0.5Hz 
-% 3. External electrical: 50 or 60 Hz (A/C mains or line frequency)
-% 4. Other electrical: Typically >10Hz 
-% 
-% Kinds of filtering required:
-% 1. Notch filter: To eliminate line frequency 
-%
-% This code does the following:
-% The data is dvided into 3 sets, the training set, the cross-validation
-% set and the test set
-%
-% For the Training set:
-% Step 1: Bandpass filtering of ECG (we use 5 to 20 Hz). 20Hz because the
-% variations of power spectrum of the ECG is observed to be 0-20Hz in the
-% frequency domain
-% (https://www.sciencedirect.com/science/article/pii/S0898122107005019)
-% also from
-% (http://www.ems12lead.com/2014/03/10/understanding-ecg-filtering/)
-%
-% Step 2: Learn ampthresh from machine learning (training set). Basically
-% different ampthresh values are used and the ampthresh values with the
-% lowest cost function will be used. The lowest cost function will
-% correlate with the lowest number of anomalies 
-% Step 3: Estimated of R peaks. The difference between R peaks is sampled 
-% at regular intervals (controlled by parameter fsRRI) to generate the RRI.
-% Cubic spline interpolation is the preferred method of interpolation.
-% 
-% For the Cross-Validation Set:
-% This is used to check values such as the learning rate, regularisation
-% parameter et cetera 
-%
-% For the Test set:
-% The data is treated in the similar manner. The user is presented with a
-% plot of the final RRI with detected anomalies, and a plot of the RRI with anomalies removed
-% -------------------------------------------------------------------------
-% Required inputs:
-% 
-%       xECG  ~  An ECG time series. The expected voltage range is microvolt.
-%                At least 5 s of ECG is required to estimate the RRI.
-%       fsECG ~  The sampling frequency of the ECG. Recordings with
-%                sampling frequencies below 32 Hz will be rejected.
-%                (Insufficient for analysis)
-% 
-% -------------------------------------------------------------------------
-% Outputs:
-% 
-%       xRRI  ~  The RR interval (RRI) time series, sampled at regular
-%                intervals.
-%       fsRRI ~  The sampling frequency of the RRI. Recordings with
-%       sampling frequency below 32 Hz will be rejected (because of the
-%       Nyuqist Thereom?)
-% -------------------------------------------------------------------------
-% Parameters:
-% 
-%       Hp             ~    High pass ECG filter cutoff (in Hz).
-%       Lp             ~    Low pass ECG filter cutoff (in Hz).
-%       ampthresh      ~    Minimum R peak amplitude. 
-%       ampthresh_autodetect ~ 'Y' (default) or 'N'. The user can let the function 
-%                           pick an appropriate value for 'ampthresh'.   
-%       timethresh     ~    Minimum time distance between R peaks (in s). 
-%       fsRRI          ~    The sampling frequency of the RRI.
-%       anomalyparam   ~    Controls the sensitivity of the anomaly detection
-%                           algorithm. The larger the value, the less
-%                           sensitive the algorithm.
-%       AAR            ~    Automatic anomaly removal, 'N' (default) or
-%       'Y'.
-%                           When set to 'Y', all deteted anomalies are removed.           
-% 
-% Parameter values can be changed from their default values in the following
-% way:
-%          [xRRI,fsRRI]=ECG_to_RRI(xECG,fsECG,'timethresh',0.5);
-% 
-% In the above, the user has selected the parameter 'timethresh' as 0.5 s.
-% -------------------------------------------------------------------------
-% -------------------------------------------------------------------------
-% load inputs and parameters
+function [meanh, anomalyvector2] =ECG_to_RRIadapt(varargin)
 
 
-[xECG,fsECG,hpECG,lpECG,fsRRI,ampthresh,timethresh,anomalyparam,ampthresh_autodetect,AAR] = init(varargin{:});
-%time of parameters that can be loaded into the function
+xECG = varargin{1};
+fsECG = varargin{2};
+%xECG=str2num(datamatrix);
+
+if size(xECG,1) > 1
+  xECG = xECG.';
+end
+
+if ~isscalar(fsECG)
+  error('''fsECG'' must be a scalar.')
+end
+
+if (fsECG)<32
+  error('The ECG sampling frequency is insufficiently low for analysis (<32 Hz).')
+end
+
+if (fsECG)<120
+  disp('Warning. The ECG sampling frequency is below the recommended value (<120 Hz).')
+end
+
+if ceil(length(xECG)/fsECG)<10
+  error('At least 10 s of ECG are required to calculate RRI.')
+end
+
+if nargin>2
+    inopts = struct(varargin{3:end});
+end
+parameter_fields = {'hpECG','lpECG','fsRRI','ampthresh','anomalyparam','timethresh','ampthresh_autodetect','AAR'};
+
+% default parameter values
+default_params.hpECG = 5; % high cut-off frequency
+default_params.lpECG = 20; % low cut-off frequency
+default_params.fsRRI = 4;
+default_params.ampthresh = 100; 
+default_params.timethresh = 0.4; 
+default_params.anomalyparam = 0.7;
+default_params.ampthresh_autodetect = 'Y';
+default_params.AAR = 'N'; % areas at risk 
+opts = default_params;
+
+if nargin==2
+    inopts = default_params;
+end
+
+parameters = fieldnames(inopts);
+
+for parameter = parameters'
+  if ~any(strcmpi(char(parameter), parameter_fields))
+    error(['parameter not recognized: ',char(parameter)])
+  end
+  if ~isempty(eval(['inopts.',char(parameter)])) 
+    eval(['opts.',(char(parameter)),' = inopts.',char(parameter),';'])
+  end
+end
+
+hpECG = opts.hpECG;
+lpECG = opts.lpECG;
+fsRRI = opts.fsRRI;
+ampthresh = opts.ampthresh;
+timethresh = opts.timethresh;
+anomalyparam = opts.anomalyparam;
+AAR = opts.AAR;
+if (any(strcmp(varargin,'ampthresh')))&&(any(strcmp(varargin,'ampthresh_autodetect')))
+    %disp('Warning. Parameters ''ampthresh'' and ''ampthresh_autodetect'' have BOTH been selected. Auto-detection of the amplitude threshold has been turned OFF.')
+    ampthresh_autodetect = 'N';
+elseif (any(strcmp(varargin,'ampthresh')))
+    ampthresh_autodetect = 'N';
+    %disp('A parameter value for ''ampthresh'' has been selected. Auto-detection of the amplitude threshold has been turned OFF.')
+else
+    ampthresh_autodetect = opts.ampthresh_autodetect;
+end
 
 % perform inversion test=> it's a function
-xECG = inversion_test(xECG,fsECG);
+% generate highpass filter coefficients of 4th order highpass digital
+% Butterworth filter with normalised cutoff frequency of 1 Hz
+[b,a]=butter(4,[1]/(fsECG/2),'high');
+
+% carry out zero-phase digital filtering to reduce noise in the signal and
+% preserve the QRS complex at the same time it occurs at the original 
+
+% carry out zero-phase digital filtering the with 4th order high-pass
+% Butterworth filter. x is ECG samples 
+y=filtfilt(b,a,xECG);
+
+% skewness(y) <0 means that data is spread out more to the left of the mean
+% than to the right, this means that ECG could have negative values
+%test for inversion
+if skewness(y)<0 %ECG may be inverted, change the sign of the ECG
+    xECG=xECG*-1;
+    disp('Sign of ECG has been changed');
+end
+
 % generate filter coefficients
 try
     % pass through a bandpass filter with a lower cutoff frequency of hpECG
@@ -107,19 +107,94 @@ catch
 end
 % filter ECG with zero-phase digital filtering using a 4th order bandpass
 % butterworth filter
-yECG=filtfilt(b,a,xECG);
+xECG=filtfilt(b,a,xECG);
 
 % if ampthresh_autodetect is selected, the amplitude threshold is selected
 % in an automatic fashion
 % See section on default parameter values. 
-if strcmp(ampthresh_autodetect,'Y')
-    ampthresh=3.4*std(yECG);
-end
-
+%if strcmp(ampthresh_autodetect,'Y')
+ %   ampthresh=3.4*std(xECG);
+%end
 % calculate RRI
-[~,xRRI, ANOMALIES]=get_RRI(yECG,fsECG,fsRRI,ampthresh,timethresh,anomalyparam,AAR);
 
+%get the sample duration of the ECG
+T=[1:length(xECG)]/fsECG;
+
+%get the sample duration in intervals of 1/RRI_fs to plot the RRI graph. Default RRI_fs is 4Hz.
+%T= 1/f
+TQ=[1:T(end)*fsRRI]/fsRRI;
+
+% min peak distance in sample number
+timethresh = ceil(timethresh*fsECG);
+ 
+% find peaks in ECG. Restrict acceptable peak-to-peak separations to values
+% greater than minpeakdistance. Peaks must be greater than minpeakheight. 
+%y_peak refers to the height of the peaks, T_peak refers to the indices at which the peaks occur 
+[y_peak,T_peak] = findpeaks(xECG,'MINPEAKDISTANCE',timethresh,'MINPEAKHEIGHT',ampthresh);
+
+
+% divide by fs to get the location in time
+T_peak=T_peak/fsECG; 
+
+%this only works when the anomaly is low
+RR_unfiltered=((diff((T_peak))));
+ 
+%median is better for skewed data
+RR_median=median(RR_unfiltered);
+
+ANOMALIES= zeros(length(RR_unfiltered),1);
+
+if isempty(T_peak)
+    disp('Warning. No peaks were identified. Anomaly is set to 1')
+    %show full anomaly
+    ANOMALIES=ones(10,1);
+    xRRI=0; %set RR to zero
+else
+    %duration of the sample
+    number_seconds=ceil(length(xECG)/fsECG);
+    %find number of peaks
+    num_peaks=length(y_peak);
+    %calculate beats per minute
+    bpm=ceil(60*num_peaks/number_seconds);    
+    
+    if bpm<20
+        disp('Warning. A small number of peaks were identified. Recommend adjusting the parameters. Anomalies are set to 1')
+        %assign a value of 10 ones
+        ANOMALIES=ones(10,1);
+        xRRI=0; %set RR to zero
+    elseif bpm>300
+        disp('Warning. A large number of peaks were identified. Recommend adjusting the parameters.')
+        ANOMALIES=ones(10,1);
+        xRRI=0; %set RR to zero
+    else
+        USER_SATISFACTION=0;
+
+    %if any of the time intervals deviates too far away from the median, it is
+    %classified as an anomaly
+
+        for n=1:1:length(RR_unfiltered)
+            if (RR_unfiltered(n)> (1+anomalyparam) * RR_median || RR_unfiltered(n) < (1-anomalyparam)* RR_median)
+                ANOMALIES(n)=1;
+                
+            %loop through all the anomalies to test if it is 
+            end 
+           
+        end 
+    % 1D interpolation using shape-preserving cubic interpolation
+    % TQ is the sample duration in intervals of 1/RRI_fs to plot the RRI graph. Default RRI_fs is 4Hz.
+    %T= 1/f 
+    %check if data has an error, if it does, assign ANOMALIES=1
+    %check if T_peak is empty
+
+    xRRI=interp1(T_peak(1:end-1),RR_unfiltered,TQ,'PCHIP');
+    end 
+end 
+
+anomalyvector2=sum(ANOMALIES);
+hmatrix=60./xRRI;
+meanh=mean(hmatrix);
+ 
 end 
 
 
-
+ 
